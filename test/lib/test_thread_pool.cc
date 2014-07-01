5a22dab753d1ae6e63625b1a80903a3201c379c0
Kiyoshi Ikehara <kiyoshi.ikehara@gree.net>
2014-07-01 15:50:57 +0900

    Add test_get_thread unit test to test_thread_pool

--- test/lib/test_thread_pool.cc
+++ test/lib/test_thread_pool.cc
@@ -8,14 +8,36 @@
 
 #include <app.h>
 #include <thread_pool.h>
+#include <pthread.h>
 
 using namespace gree::flare;
 
 namespace test_thread_pool
 {
+	void sa_usr1_handler(int sig) {
+		// just ignore
+	}
+
+	class thread_handler_test : public thread_handler {
+	public:
+		thread_handler_test(shared_thread t): thread_handler(t) {};
+		int run () { sleep(2); return 0; };
+	};
+
+	struct sigaction prev_sigusr1_action;
+
 	void setup() {
 		stats_object = new stats();
 		stats_object->update_timestamp();
+		
+		struct sigaction sa;
+		// SIGUSR1
+		memset(&sa, 0, sizeof(sa));
+		sa.sa_handler = sa_usr1_handler;
+		if (sigaction(SIGUSR1, &sa, &prev_sigusr1_action) < 0) {
+			// TODO: show a error message or throw a error here.
+			return;
+		}
 	}
 
 	struct thread_pool_test : public thread_pool
@@ -28,6 +50,16 @@ namespace test_thread_pool
 		using thread_pool::_global_map;
 		using thread_pool::_pool;
 	};
+
+	void test_get_thread() {
+		// Preparation
+		thread_pool_test pool;
+		shared_thread t = pool.get(1);
+		thread_handler_test *th = new thread_handler_test(t);
+		t->trigger(th);
+		sleep(1);
+		t->shutdown();
+	}
 
 	void test_duplicate_thread_id_different_types() {
 		// Preparation
@@ -61,6 +93,11 @@ namespace test_thread_pool
 
 	void teardown()
 	{
+		// SIGUSR1
+		if (sigaction(SIGUSR1, &prev_sigusr1_action, NULL) < 0) {
+			// TODO: show a error message or throw a error here.
+			return;
+		}
 		delete stats_object;
 	}
 }
